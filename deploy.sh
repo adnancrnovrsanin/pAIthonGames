c11bfd1271439ae5d263eb327cbeae35d494bf01
Adnan Crnovrsanin <actaer@pop-os.localdomain>
2023-01-08 16:35:14 +0100

    Changes

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:adnancrnovrsanin/pAIthonGames.git main:gh-pages
+# git push -f git@github.com:adnancrnovrsanin/pAIthonGames.git main:gh-pages
+git subtree push --prefix dist origin gh-pages
 
 cd -
