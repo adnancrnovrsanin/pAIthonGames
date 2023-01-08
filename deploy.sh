3396f45c0e17964f86e4be9d326e8d56b5757b40
Adnan Crnovrsanin <actaer@pop-os.localdomain>
2023-01-08 16:40:49 +0100

    Changes

--- deploy.sh
+++ deploy.sh
@@ -15,9 +15,7 @@ echo > .nojekyll
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git init
-git checkout -B main
-git add -A
+git add .
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
