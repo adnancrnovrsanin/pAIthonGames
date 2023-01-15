b2939accadfed7afed15dbf5a017c9b352e2e90a
Adnan Crnovrsanin <actaer@pop-os.localdomain>
2023-01-15 17:15:53 +0100

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,18 @@ import { Route, Routes } from 'react-router-dom'
 import PyTanja from './pages/PyTanja'
 import PyStolovina from './pages/PyStolovina'
 import Home from './pages/Home'
+import { observer } from 'mobx-react-lite'
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/pytanja" element={<PyTanja />} />
-        <Route path="/pystolovina" element={<PyStolovina />} />
+        <Route path="" element={<Home />} />
+        <Route path="pytanja" element={<PyTanja />} />
+        <Route path="pystolovina" element={<PyStolovina />} />
       </Routes>
     </div>
   )
 }
 
-export default App
+export default observer(App);
