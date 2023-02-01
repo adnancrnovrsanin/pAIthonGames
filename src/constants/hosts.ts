f8a01b4b26c73294af88867af5be3b8cc8799390
Actaer2001 <adnancrnovrsanin48@gmail.com>
2023-02-01 19:21:30 +0100

    Minor fixes

--- src/constants/hosts.ts
+++ src/constants/hosts.ts
@@ -1,2 +1,2 @@
-// export const APIBase = 'https://adnancrnovrsanin.pythonanywhere.com/'; 
-export const APIBase = 'http://localhost:8000/';
+export const APIBase = 'https://adnancrnovrsanin.pythonanywhere.com/'; 
+// export const APIBase = 'http://localhost:8000/';
